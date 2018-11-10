202feb9f3ab397bf8296e8c8d7e3761a5ecf6580
Vagner Sabadi <vagnerbuzatta@hotmail.com>
2018-11-10 18:07:50 -0200

    Flutter MovieApp - Tutorial #4

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,17 @@ import 'dart:convert';
 import './models/movieModel.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+/**
+ * Varaveis globais
+ */
 const baseUrl = "https://api.themoviedb.org/3/movie/";
 const baseImageUrl = "https://image.tmdb.org/t/p/";
 const apiKey = "aee0b4ecdda95b5452eb6648073fd6c6";
 
 const nowPlayingUrl = "${baseUrl}now_playing?api_key=$apiKey";
+const upcomingUrl = "${baseUrl}upcoming?api_key=$apiKey";
+const popularUrl = "${baseUrl}popular?api_key=$apiKey";
+const topRatedUrl = "${baseUrl}top_rated?api_key=$apiKey";
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -22,16 +28,32 @@ class MyMovieApp extends StatefulWidget {
   _MyMovieApp createState() => new _MyMovieApp();
 }
 
+/**
+ * Classe principal
+ */
 class _MyMovieApp extends State<MyMovieApp> {
+  //Variaveis
   Movie nowPlayingMovies;
+  Movie upcomingMovies;
+  Movie popularMovies;
+  Movie topRatedMovies;
+  int heroTag = 0;
 
   @override
   void initState() {
     super.initState();
+    /**
+     * Inicializa funcções de buscas dos filmes
+     */
     _fetchNowPlayingMovies();
+    _fetchUpcomingMovies();
+    _fetchPopularMovies();
+    _fetchTopRatedMovies();
   }
 
-  //busca filmes forma assincrona
+  /**
+   * Busca os filmes assync
+   */
   void _fetchNowPlayingMovies() async {
     var response = await http.get(nowPlayingUrl);
     var decodeJson = jsonDecode(response.body);
@@ -41,17 +63,117 @@ class _MyMovieApp extends State<MyMovieApp> {
     });
   }
 
-//carosel de imagens
+  void _fetchUpcomingMovies() async {
+    var response = await http.get(upcomingUrl);
+    var decodeJson = jsonDecode(response.body);
+    setState(() {
+      upcomingMovies = Movie.fromJson(decodeJson);
+    });
+  }
+
+  void _fetchPopularMovies() async {
+    var response = await http.get(popularUrl);
+    var decodeJson = jsonDecode(response.body);
+    setState(() {
+      popularMovies = Movie.fromJson(decodeJson);
+    });
+  }
+
+  void _fetchTopRatedMovies() async {
+    var response = await http.get(topRatedUrl);
+    var decodeJson = jsonDecode(response.body);
+    setState(() {
+      topRatedMovies = Movie.fromJson(decodeJson);
+    });
+  }
+
+/**
+ * Carousel de imagens
+*/
   Widget _buildCarouselSlider() => CarouselSlider(
-        items: nowPlayingMovies.results
-            .map((movieItem) =>
-                Image.network("${baseImageUrl}w342${movieItem.posterPath}"))
-            .toList(),
+        items: nowPlayingMovies == null
+            ? <Widget>[Center(child: CircularProgressIndicator())]
+            : nowPlayingMovies.results.map((movieItem) =>
+                //Image.network("${baseImageUrl}w342${movieItem.posterPath}")
+                _buildMovieItem(movieItem)).toList(),
         autoPlay: false,
         height: 240.0,
         viewportFraction: 0.5,
       );
 
+  Widget _buildMovieItem(Results movieItem) {
+    heroTag += 1;
+    return Material(
+      elevation: 15.0,
+      child: InkWell(
+        onTap: () {},
+        child: Hero(
+          tag: heroTag,
+          child: Image.network("${baseImageUrl}w342${movieItem.posterPath}",
+              fit: BoxFit.cover),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMovieListItem(Results movieItem) => Material(
+        child: Container(
+          width: 128.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(6.0),
+                  child: _buildMovieItem(movieItem)),
+              Padding(
+                padding: EdgeInsets.only(left: 6.0, top: 2.0),
+                child: Text(movieItem.title,
+                    style: TextStyle(fontSize: 8.0),
+                    overflow: TextOverflow.ellipsis),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 6.0, top: 2.0),
+                  child: Text(
+                    movieItem.releaseDate,
+                    style: TextStyle(fontSize: 8.0),
+                  ))
+            ],
+          ),
+        ),
+      );
+
+ Widget _buildMoviesListView(Movie movie, String movieListTitle) => Container(
+        height: 258.0,
+        padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.4),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 7.0, bottom: 7.0),
+              child: Text(movieListTitle,
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[400])),
+            ),
+            Flexible(
+                child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: movie == null
+                  ? <Widget>[Center(child: CircularProgressIndicator())]
+                  : movie.results
+                      .map((movieItem) => Padding(
+                            padding: EdgeInsets.only(left: 6.0, right: 2.0),
+                            child: _buildMovieListItem(movieItem),
+                          ))
+                      .toList(),
+            ))
+          ],
+        ),
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -103,24 +225,21 @@ class _MyMovieApp extends State<MyMovieApp> {
                           color: Colors.blue.withOpacity(0.5)),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(top: 35.0),
-                      child: Column(
-                        children: nowPlayingMovies == null
-                            ? <Widget>[
-                                Center(
-                                  child: CircularProgressIndicator(),
-                                )
-                              ]
-                            : <Widget>[_buildCarouselSlider()],
-                      ),
-                    )
+                        padding: const EdgeInsets.only(top: 35.0),
+                        child: _buildCarouselSlider())
                   ],
                 ),
               ),
             )
           ];
         },
-        body: Text("Scroll Me!"),
+        body: ListView(
+          children: <Widget>[
+            _buildMoviesListView(upcomingMovies, 'COMING SOON'),
+            _buildMoviesListView(popularMovies, 'POPULAR'),
+            _buildMoviesListView(topRatedMovies, 'TOP RATED'),
+          ],
+        ),
       ),
     );
   }
